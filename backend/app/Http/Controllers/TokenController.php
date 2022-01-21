953769956e8a06c6651e55eae182a3d0a8847b69
Fabrício Costa da Silva <fabricio.silva@trt4.jus.br>
2022-01-21 13:56:41 -0300

    Correção no controle de token

--- backend/app/Http/Controllers/TokenController.php
+++ backend/app/Http/Controllers/TokenController.php
@@ -54,7 +54,7 @@ class TokenController extends Controller {
 
     public function revogarToken(Request $request) {
         $accessToken = $this->obterTokenDoCabecalho($request);
-        $refreshToken = $request->REFRESH_TOKEN;
+        $refreshToken = $request->refresh_token;
 
         try {
             $dadosAccessToken = JWT::decode($accessToken, env('JWT_KEY'), ['HS256']);
@@ -102,7 +102,7 @@ class TokenController extends Controller {
 
     public function atualizarToken(Request $request) {
         try {
-            $refreshToken = $request->REFRESH_TOKEN;
+            $refreshToken = $request->refresh_token;
             if (!$this->validarToken($refreshToken)) {
                 throw new \Exception('Token inválido');
             }
